f2c67871d28ab2882fe5713a9c11ca167498c04b
unknown <omar.garciaf247@gmail.com>
2019-01-06 03:42:55 -0500

    Update

--- js/typeme.js
+++ js/typeme.js
@@ -1,6 +1,6 @@
 var typed = $(".typed");
 
-$(function() {
+$(() => {
 	typed.typed({
 	  strings: ["programming.", "singing.", "playing guitar.", "making designs.", "waiting for a bus.", "making memes."],
 	  typeSpeed: 120,
