3f234a66a35a6bdc2e26a92b161756d04f802b20
Omar Flores <omar.garciaf247@gmail.com>
2019-02-07 00:49:06 -0500

    ui updatE

---
+++ js/memes.js
@@ -0,0 +1,22 @@
+// var home = "./img/memes";
+
+// fs.readdir(home, function (err, files) {
+//   if (err) {
+//     console.error("Could not list the directory.", err);
+//     process.exit(1);
+//   }
+
+//   files.forEach(function (file, index) {
+//     // Make one pass and make the file complete
+//     var fromPath = path.join(moveFrom, file);
+
+//     fs.stat(fromPath, function (error, stat) {
+//       if (error) {
+//         console.error("Error stating file.", error);
+//         return;
+//       } else {
+//           console.log(file)
+//       }
+//     });
+//   });
+// });
