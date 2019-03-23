2897deec6d4c13d716a86224af5f91f134d9607a
Omar Flores <omar.garciaf247@gmail.com>
2019-03-23 10:16:58 -0400

    update

--- js/typeme.js
+++ js/typeme.js
@@ -2,7 +2,7 @@ var typed = $(".typed");
 
 $(() => {
 	typed.typed({
-	  strings: ["programming.", "singing.", "playing guitar.", "making designs.", "waiting for a bus.", "making memes."],
+	  strings: ["programming.", "singing.", "playing guitar.", "making designs.", "waiting for a bus."],
 	  typeSpeed: 120,
 	  loop: true,
 	});
