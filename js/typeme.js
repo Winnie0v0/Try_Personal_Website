53623c9b11a7fe0f247f80b2f3df09a3f1fa0c70
Winnie Zong <wzong@smith.edu>
2020-01-14 11:52:48 +0800

    Hi

--- js/typeme.js
+++ js/typeme.js
@@ -5,9 +5,13 @@ var
   function Chinese(){
       E=false;
       x=document.getElementById("hi")
-      x.innerHTML="Hi, I'm &#10; 宗筱雯", "我想"
+      x.innerHTML="Hi, I'm &#10; 宗筱雯"
       x1=document.getElementById("I")
 	  x1.innerHTML="我想"
+	  x2=document.getElementById("intro")
+	  x2.innerHTML="我目前是美国史密斯学院大一的学生，主修计算机专业。"
+	  x3=document.getElementById("intro")
+	  x3.innerHTML="我目前是美国史密斯学院大一的学生，主修计算机专业。"
 	  $(() => {
 		if(E==true){
 			typed.typed({
@@ -32,6 +36,8 @@ var
       x.innerHTML="Hi, I'm &#10; Winnie."
       x1=document.getElementById("I")
 	  x1.innerHTML="I am likely"
+	  x2=document.getElementById("intro")
+	  x2.innerHTML="I am currently in my first year of the Computer Science Master's Degree at Smith College in Northampton, MA."
 	  $(() => {
 		if(E==true){
 			typed.typed({
