0d46d40ac975b60f8005a17f19e1440269b0490a
Winnie Zong <wzong@smith.edu>
2020-01-15 14:28:46 +0800

    hhhhhh

--- js/typeme.js
+++ js/typeme.js
@@ -3,7 +3,7 @@ var
 	E=false
 
   function Chinese(){
-      E=false;
+
       x=document.getElementById("hi")
       x.innerHTML="Hi, I'm &#10; 宗筱雯"
       x1=document.getElementById("I")
@@ -28,27 +28,55 @@ var
 	  x10.innerHTML="SDS192课程: 学习R语言"
 	  x11=document.getElementById("aca_5")
 	  x11.innerHTML="点击这里看看我的<a href=''>期末成果展示</a>!"
+
+	  x12=document.getElementById("exp_1")
+      x12.innerHTML="<img class='img-fluid' src='cat2.png' alt='' width='35' /> 经历与经验"
+      x13=document.getElementById("exp_2")
+      x13.innerHTML="核心成员, 史密斯学院中国学生会"
+      x14=document.getElementById("exp_3")
+      x14.innerHTML="组织活动。"
+      x15=document.getElementById("exp_4")
+      x15.innerHTML="实习生, 北京市朝阳区永续全球环境研究所"
+      x16=document.getElementById("exp_5")
+      x16.innerHTML="编辑，研究，分析数据，制作环境保护手册。"
+      x17=document.getElementById("exp_6")
+      x17.innerHTML="核心成员, Girls Who Code"
+      x18=document.getElementById("exp_7")
+      x18.innerHTML="教授课程。"
+      x19=document.getElementById("exp_8")
+      x19.innerHTML="核心成员, 史密斯学院计算机社"
+      x20=document.getElementById("exp_9")
+	  x20.innerHTML="积极参与活动。"
+	  
+	  x21=document.getElementById("hac_1")
+      x21.innerHTML="<img class='img-fluid' src='fish2.png' alt='' width='35' /> 黑客马拉松"
+      x22=document.getElementById("hac_2")
+      x22.innerHTML="积极参与教授讲座。"
+      x23=document.getElementById("hac_3")
+      x23.innerHTML="Hackholyoke 2019"
+      x24=document.getElementById("hac_4")
+      x24.innerHTML="未完待续..."
+      x25=document.getElementById("hac_5")
+      x25.innerHTML="Hack(H)er413 2020"
+
+      x26=document.getElementById("con")
+      x26.innerHTML="<img class='img-fluid' src='paw4.png' alt='' width='50' /> 让我们一起努力! <img class='img-fluid' src='cat2.png' alt='' width='65' />"
+
 	  
 	  $(() => {
-		if(E==true){
-			typed.typed({
-				strings: ["programming.", "designing.",  "sketching.", "on my way to somewhere."],
-				typeSpeed: 140,
-				loop: true,
-			  });
-			}
-		else{
-			  typed.typed({
-				strings: ["我在编程。", "我在设计。",  "我在绘画。", "我在前往未知的路上。"],
-				typeSpeed: 140,
-				loop: true,
-			  });
-			}	
-	});
-  }
+		typed.typed({
+		  strings: ["我在编程。", "我在设计。",  "我在绘画。", "我在前往未知的路上。"],
+		  typeSpeed: 140,
+		  loop: true,
+		});
+});
+}
+	  
+
+	  
     
   function English(){
-      E=true;
+
       x=document.getElementById("hi")
       x.innerHTML="Hi, I'm &#10; Winnie."
       x1=document.getElementById("I")
@@ -73,39 +101,57 @@ var
 	  x10.innerHTML="SDS192: Learning R"
 	  x11=document.getElementById("aca_5")
 	  x11.innerHTML="Click here for the markdown of our <a href=''>final project</a>!"
-	  $(() => {
-		if(E==true){
-			typed.typed({
-				strings: ["programming.", "designing.",  "sketching.", "on my way to somewhere."],
-				typeSpeed: 140,
-				loop: true,
-			  });
-			}
-		else{
-			  typed.typed({
-				strings: ["我在编程。", "我在设计。",  "我在绘画。", "我在前往未知的路上。"],
-				typeSpeed: 140,
-				loop: true,
-			  });
-			}	
-	});
-  }
 
-  $(() => {
-	if(E==true){
+	  x12=document.getElementById("exp_1")
+      x12.innerHTML="<img class='img-fluid' src='cat2.png' alt='' width='35' /> Experience"
+      x13=document.getElementById("exp_2")
+      x13.innerHTML="E-board Member, Smith Chinese Student Accosiation"
+      x14=document.getElementById("exp_3")
+      x14.innerHTML="Organize events."
+      x15=document.getElementById("exp_4")
+      x15.innerHTML="Intern, Global Environment Institute (Beijing)"
+      x16=document.getElementById("exp_5")
+      x16.innerHTML="Researched, edited, and illustrated informational booklet for distribution to fishermen."
+      x17=document.getElementById("exp_6")
+      x17.innerHTML="Member, Girls Who Code"
+      x18=document.getElementById("exp_7")
+      x18.innerHTML="Lead classes."
+      x19=document.getElementById("exp_8")
+      x19.innerHTML="Active Member, Smith in CS"
+      x20=document.getElementById("exp_9")
+	  x20.innerHTML="Attend Events"
+
+	  x21=document.getElementById("hac_1")
+      x21.innerHTML="<img class='img-fluid' src='fish2.png' alt='' width='35' /> Hackathon Projects"
+      x22=document.getElementById("hac_2")
+      x22.innerHTML="Attend web build workshop."
+      x23=document.getElementById("hac_3")
+      x23.innerHTML="Hackholyoke 2019"
+      x24=document.getElementById("hac_4")
+      x24.innerHTML="To be continue..."
+      x25=document.getElementById("hac_5")
+      x25.innerHTML="Hack(H)er413 2020"
+
+      x26=document.getElementById("con")
+      x26.innerHTML="<img class='img-fluid' src='paw4.png' alt='' width='50' /> Let's work together. <img class='img-fluid' src='cat2.png' alt='' width='65' />"
+	  
+	  $(() => {
 		typed.typed({
 			strings: ["programming.", "designing.",  "sketching.", "on my way to somewhere."],
 			typeSpeed: 140,
 			loop: true,
 		  });
-		}
-	else{
-		  typed.typed({
-			strings: ["我在编程。", "我在设计。",  "我在绘画。", "我在前往未知的路上。"],
-			typeSpeed: 140,
-			loop: true,
-		  });
-		}	
 });
+}
+
+
+$(() => {
+	typed.typed({
+	  strings: ["我在编程。", "我在设计。",  "我在绘画。", "我在前往未知的路上。"],
+	  typeSpeed: 140,
+	  loop: true,
+	});
+});
+
 
 
